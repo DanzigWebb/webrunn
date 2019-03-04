940473a190a6ae1f93d0010354d7b40e45abd0ce
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-03-05 00:28:39 +0300

    full

--- assets/js/myScripts.js
+++ assets/js/myScripts.js
@@ -2,15 +2,14 @@
 function openSidebar () {
   var buttonMenu = document.querySelector('.my-menu-button');
   var sidebar = document.querySelector('.mysidebar');
-    console.log(buttonMenu)
 
 
   buttonMenu.onclick = function(e) {
+    // задержка анимации гамбургера
     function func() {
       buttonMenu.classList.toggle('is-active');
     };
     setTimeout(func, 400)
-
     sidebar.classList.toggle('is-active');
   }
 }
